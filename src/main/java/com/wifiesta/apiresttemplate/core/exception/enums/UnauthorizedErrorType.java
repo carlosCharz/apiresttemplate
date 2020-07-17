f07e2224398d493a13a2f38bd37359b4b6e248e5
carlosCharz <carlos.becerra1234@gmail.com>
2020-07-17 15:41:27 -0500

    refactor unauthorize status

--- src/main/java/com/wifiesta/apiresttemplate/core/exception/enums/UnauthorizedErrorType.java
+++ src/main/java/com/wifiesta/apiresttemplate/core/exception/enums/UnauthorizedErrorType.java
@@ -6,12 +6,9 @@ package com.wifiesta.apiresttemplate.core.exception.enums;
  * @author charz
  */
 public enum UnauthorizedErrorType {
-    UNAUTHORIZED(1, "Unauthorized application access"),
-    INCORRECT_CREDENTIALS(2, "Unauthorized! Incorrect iclass credentials"),
-    UNAUTHORIZED_DUE_TO_AUTHORIZATION_PARAM(3, "Missing authorization param in the header"),
-    UNAUTHORIZED_DUE_TO_TOKEN_NOT_FOUND(4, "Unauthorized! Access token not found!"),
-    UNAUTHORIZED_DUE_TO_MISMATCH_USER(5,
-            "Unauthorized! The user that makes the request does not match with the access token associated with him!");
+    UNAUTHORIZED(1, "Unauthorized application access."),
+    INCORRECT_REQUEST_CREDENTIALS(2, "Unauthorized! Incorrect request credentials."),
+    MISSING_AUTHORIZATION_PARAM(3, "Missing authorization param in the header.");
 
     private final int code;
     private final String message;
